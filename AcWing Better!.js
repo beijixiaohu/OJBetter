6fbf2f6ee251f5b48cf11974355d2825f264039e
beijixiaohu <yuhao888123@gmail.com>
2023-09-17 18:40:28 +0800

    add
    添加ac计时器功能

--- AcWing Better!.js
+++ AcWing Better!.js
@@ -32,6 +32,7 @@ const bottomBar = getGMValue("bottomBar", true);
 const bingWallpaper = getGMValue("bingWallpaper", true);
 const widthAdjustment = getGMValue("widthAdjustment", true);
 const autoPlay = getGMValue("autoPlay", true);
+const acTimer = getGMValue("acTimer", true);
 
 // 常量
 const helpCircleHTML = '<div class="help-icon"><svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg"><path fill="currentColor" d="M512 64a448 448 0 1 1 0 896 448 448 0 0 1 0-896zm23.744 191.488c-52.096 0-92.928 14.784-123.2 44.352-30.976 29.568-45.76 70.4-45.76 122.496h80.256c0-29.568 5.632-52.8 17.6-68.992 13.376-19.712 35.2-28.864 66.176-28.864 23.936 0 42.944 6.336 56.32 19.712 12.672 13.376 19.712 31.68 19.712 54.912 0 17.6-6.336 34.496-19.008 49.984l-8.448 9.856c-45.76 40.832-73.216 70.4-82.368 89.408-9.856 19.008-14.08 42.24-14.08 68.992v9.856h80.96v-9.856c0-16.896 3.52-31.68 10.56-45.76 6.336-12.672 15.488-24.64 28.16-35.2 33.792-29.568 54.208-48.576 60.544-55.616 16.896-22.528 26.048-51.392 26.048-86.592 0-42.944-14.08-76.736-42.24-101.376-28.16-25.344-65.472-37.312-111.232-37.312zm-12.672 406.208a54.272 54.272 0 0 0-38.72 14.784 49.408 49.408 0 0 0-15.488 38.016c0 15.488 4.928 28.16 15.488 38.016A54.848 54.848 0 0 0 523.072 768c15.488 0 28.16-4.928 38.72-14.784a51.52 51.52 0 0 0 16.192-38.72 51.968 51.968 0 0 0-15.488-38.016 55.936 55.936 0 0 0-39.424-14.784z"></path></svg></div>';
@@ -844,6 +845,10 @@ const ACwingBetterSettingMenuHTML = `
         <input type="checkbox" id="autoPlay" name="autoPlay" checked>
         <label for="autoPlay">不自动播放视频</label>
     </div>
+    <div class='ACBetter_setting_list'>
+        <input type="checkbox" id="acTimer" name="acTimer" checked>
+        <label for="acTimer">开启AC计时器</label>
+    </div>
     <br>
     <button id='save'>保存</button>
     </div>
@@ -860,18 +865,21 @@ $(document).ready(function () {
         $("body").append(ACwingBetterSettingMenuHTML);
 
         addDraggable($('#ACwingBetter_setting_menu'));
+        $("#bottomBar").prop("checked", GM_getValue("bottomBar"));
+        $("#bingWallpaper").prop("checked", GM_getValue("bingWallpaper"));
+        $("#widthAdjustment").prop("checked", GM_getValue("widthAdjustment"));
+        $("#autoPlay").prop("checked", GM_getValue("autoPlay"));
+        $("#acTimer").prop("checked", GM_getValue("acTimer"));
+
         $("#save").click(function () {
             GM_setValue("bottomBar", $("#bottomBar").prop("checked"));
             GM_setValue("bingWallpaper", $("#bingWallpaper").prop("checked"));
             GM_setValue("widthAdjustment", $("#widthAdjustment").prop("checked"));
             GM_setValue("autoPlay", $("#autoPlay").prop("checked"));
+            GM_setValue("acTimer", $("#acTimer").prop("checked"));
             $(styleElement).remove();
             location.reload();
         });
-        $("#bottomBar").prop("checked", GM_getValue("bottomBar"));
-        $("#bingWallpaper").prop("checked", GM_getValue("bingWallpaper"));
-        $("#widthAdjustment").prop("checked", GM_getValue("widthAdjustment"));
-        $("#autoPlay").prop("checked", GM_getValue("autoPlay"));
         // 关闭
         $("#ACwingBetter_setting_menu .btn-close").click(function () {
             $("#ACwingBetter_setting_menu").remove();
@@ -1039,7 +1047,7 @@ function codeCopy() {
 
 function addConversionButton() {
     // 添加按钮到content部分
-    if (!window.location.href.includes("update")) {
+    if (!window.location.href.includes("code")) {
         $('div[data-field-name="content"]').each(function () {
             let id = "_question-oi-bd_" + getRandomNumber(8);
             addButtonPanel(this, id, "this_level");
@@ -1225,7 +1233,53 @@ function addPlayerBar(player_bar_video) {
     $("body").append(menu);
 }
 
+function formatTime(time) {
+    var seconds = Math.floor((time / 1000) % 60);
+    var minutes = Math.floor((time / (1000 * 60)) % 60);
+    var hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+    var timeString = '';
+
+    if (days > 0) {
+        timeString += days + '天';
+    }
+
+    if (hours > 0) {
+        timeString += hours + '小时';
+    }
+
+    if (minutes > 0) {
+        timeString += minutes + '分钟';
+    }
+
+    if (seconds > 0) {
+        timeString += seconds + '秒';
+    }
+
+    return timeString;
+}
+
+// AC计时器
+function acTiming() {
+    var startTime = new Date();
+    var timer = setInterval(function () {
+        var status = $('#submit-code-status-value-id').text().trim();
+
+        if (status === 'Accepted') {
+            clearInterval(timer);
+
+            var endTime = new Date();
+            var totalTime = endTime - startTime;
+
+            var timeString = formatTime(totalTime);
+            $('#submit-code-status-value-id').after('<div class="time-info">耗时：' + timeString + ' 要重新开始计时请刷新页面</div>');
+        }
+    }, 500);
+}
+
 $(document).ready(function () {
+    if (acTimer) acTiming();
     // 让某些链接在新窗口打开
     var regExps = [
         /常用代码模板/,
