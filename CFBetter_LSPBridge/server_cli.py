549ea3291495b85a766a88763562b661f13565a6
beijixiaohu <yuhao888123@gmail.com>
2023-11-13 21:53:19 +0800

    feat(Codeforces): 更换编辑器为 monaco，新增 LSP 以及相应的 bridge server ，修复了若干问题
    - 更换题目页的代码编辑器为 monaco ，同时为其提供LSP (language server protocol) 支持
    
    - 增强编辑器代码自动保存与恢复在某些情况下的健壮性
    
    - 修复 wandbox 在线编译器切换语言时报错的问题
    
    - 移除对codechef在线编译器的支持
    
    - 改进 CF_CsrfToken 的获取方式
    
    - 完善黑暗模式，改进样例元素的 hover 样式
    
    - 改进，使 convertStatementToText 忽略 html2md-panel 元素，以解决有一定概率导致插入脚本按钮会被判定为题目描述更改的问题
    
    - 完善组合混搭管理页面的汉化
    
    - 修复 gym 题目的题目页提交 submitUrl 错误的问题
    
    - 修复对于 [a-z0-9] 这样的题目编号处理错误的问题
    
    - 增加设置面板选项: 翻译-过滤文本中的**号
    
    close #9
    
    
    Former-commit-id: f7259e3acf1319cd7ce8a9b9fe7dbd88e9abf589

---
+++ CFBetter_LSPBridge/server_cli.py
@@ -0,0 +1,248 @@
+import os
+import subprocess
+import threading
+import argparse
+import yaml
+from tornado import ioloop, process, web, websocket, httpserver
+from pylsp_jsonrpc import streams
+import ujson as json
+import pkg_resources
+
+print("\033[1;34m" + """
+    ╔══════════════════════════════════════════════════════════╗
+    ║           Welcome to CFBetter_MonacoLSPBridge            ║
+    ║                  Enjoy Coding !>_<                       ║
+    ╚══════════════════════════════════════════════════════════╝
+    """.center(80) + "\033[0m")
+
+print("[CHECK] Checking required packages...")
+REQUIRED_PACKAGES = {'PyYAML': '==6.0', 'tornado': '==6.1', 'ujson': '==5.2.0'}
+for package, version in REQUIRED_PACKAGES.items():
+    try:
+        dist = pkg_resources.get_distribution(package)
+        if dist.version != version[2:]:
+            print('\033[1;31m[CHECK] {} ({}) is installed but not the required version {}\033[0m'.format(package, dist.version, version))
+            subprocess.call(['pip', 'install', '{}{}'.format(package, version)])
+        else:
+            print('\033[1;32m[CHECK] {} ({}) is installed \u2714\033[0m'.format(package, dist.version))
+    except pkg_resources.DistributionNotFound:
+        print('\033[1;31m[CHECK] {} is NOT installed \u2718\033[0m'.format(package))
+        subprocess.call(['pip', 'install', '{}{}'.format(package, version)])
+
+class JsonRpcStreamLogWriter(streams.JsonRpcStreamWriter):
+    """
+    A class that formats json messages with the correct LSP headers.
+    """
+    def write(self, ip, message):
+        """
+        Write the message to the stream.
+        """
+        return super().write(message)
+
+
+class HomeRequestHandler(web.RequestHandler):
+    """
+    A class that handles the home request.
+    """
+    commands = None
+
+    def initialize(self, commands) -> None:
+        """
+        Initialize the class with the given commands.
+        """
+        self.commands = commands
+
+    def get(self):
+        """
+        Handle the GET request.
+        """
+        self.write("""
+        <h1>Welcome to CFBetter_MonacoLSPBridge</h1>
+        """)
+
+
+class FileServerWebSocketHandler(websocket.WebSocketHandler):
+    """
+    A class that handles the file server web socket.
+    """
+    rootUri = None
+    createdFiles = {} # the files that are created by the client
+    nowconnection = None # the connection that is now being used
+
+    def initialize(self, rootUri) -> None:
+        """
+        Initialize the class with the given root URI.
+        """
+        self.rootUri = rootUri
+
+    def open(self, *args, **kwargs):
+        """
+        Handle the open event.
+        """
+        print("\033[1;32m[INFO] A new connection about FileServer has been created\033[0m")
+
+    def on_message(self, message):
+        """
+        Handle the message event.
+        """
+        message = json.loads(message)
+        if message['type'] == 'update':
+            filename = message['filename']
+            file_extension = message['fileExtension']
+            code = message['code']
+            workspace = message['workspace']
+            file_path = os.path.join(rootUri, workspace, filename + file_extension) # the path of the file
+            self.createdFiles[self.nowconnection] = file_path
+            with open(file_path, 'w') as f:
+                f.write(code)
+            self.write_message(json.dumps({'result': 'ok'}))
+        else:
+            self.write_message(json.dumps(
+                {'result': 'error', 'description': 'no such type'}))
+
+    def on_close(self):
+        """
+        Handle the close event.
+        """
+        print("\033[1;31m[INFO] A connection about FileServer has been closed\033[0m")
+        print("\033[1;31m[INFO] Removing the file that is created by the client...\033[0m")
+        for connection in self.createdFiles:
+            if connection == self:
+                file_path = self.createdFiles[connection]
+                os.remove(file_path)
+                self.update_log_from_thread("warn", f"File {file_path} has been removed.")
+                del self.createdFiles[connection]
+                break
+
+    def check_origin(self, origin):
+        """
+        Check the origin of the request.
+        """
+        self.set_header("Access-Control-Allow-Origin", "*")
+        return True
+
+
+class LanguageServerWebSocketHandler(websocket.WebSocketHandler):
+    """
+    A class that handles the language server web socket.
+    """
+    writer = None
+    lang = None
+    commands = None
+    uri = None
+    proc = None
+    language = None
+
+    def initialize(self, commands) -> None:
+        """
+        Initialize the class with the given commands.
+        """
+        self.commands = commands
+
+    def open(self, *args, **kwargs):
+        """
+        Handle the open event.
+        """
+        self.language = args[0]
+        print("\033[1;32m[INFO] A new connection about LanguageServer has been created\033[0m")
+        if args[0] not in self.commands:
+            self.close(1001, "language {} is not supported".format((args[0])))
+            return
+
+        self.lang = args[0]
+        self.proc = process.Subprocess(
+            self.commands[self.lang], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        self.writer = JsonRpcStreamLogWriter(self.proc.stdin)
+
+        def consume():
+            """
+            Consume the stdout of the language server.
+            """
+            ioloop.IOLoop()
+            reader = streams.JsonRpcStreamReader(self.proc.stdout)
+            reader.listen(lambda msg: self.write_message(json.dumps(msg)))
+
+        thread = threading.Thread(target=consume)
+        thread.daemon = True
+        thread.start()
+        print("\033[1;32m[INFO] LanguageServerWebSocketHandler opened\033[0m")
+
+    def on_message(self, message):
+        """
+        Handle the message event.
+        """
+        message = json.loads(message)
+        self.writer.write("", message)
+
+    def on_close(self):
+        """
+        Handle the close event.
+        """
+        self.proc.proc.terminate()
+        print("\033[1;31m[INFO] A connection about LanguageServer has been closed\033[0m")
+
+    def check_origin(self, origin):
+        """
+        Check the origin of the request.
+        """
+        self.set_header("Access-Control-Allow-Origin", "*")
+        return True
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", type=str,
+                        default="config.yml", help="yaml configuration")
+    args = parser.parse_args()
+
+    file_dir_path = os.path.dirname(os.path.abspath(__file__))
+
+    config_path = os.path.join(file_dir_path, args.config)
+
+    config = None
+    with open(config_path, 'r') as f:
+        config = yaml.safe_load(f)
+
+    rootUri = os.path.dirname(os.path.abspath(__file__))
+
+    for lang in config['commands'].keys():
+        command = config['commands'][lang][0] + " --version"
+        try:
+            subprocess.run(command, shell=True, check=True, timeout=1, stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+            print(f"\033[1;32m[CHECK] {lang} command is valid \u2714\033[0m")
+        except subprocess.CalledProcessError:
+            if lang == "cpp":
+                print("""\033[1;31m[CHECK] The command for C++ is invalid. 
+            Please download clangd-windows-xxx.zip from https://github.com/clangd/clangd/releases/, 
+            extract it to your preferred location, and add the xxx/clangd_xxx/bin path to the system environment variable path.\033[0m""")
+            elif lang == "python":
+                print("""\033[1;31m[CHECK] The command for Python is invalid. 
+            Please run the command 'pip install "python-lsp-server[all]"' in the terminal 
+            (you need to have a Python environment installed), which will automatically configure the environment variables.\033[0m""")
+            elif lang == "java":
+                print("""\033[1;31m[CHECK] The command for Java is invalid. 
+            To simplify the configuration, the 'cfbetter_monaco-lsp-bridge' package already includes 'jdtls', 
+            which is in the 'jdt-language-server' folder and version '1.29.0-202310261436'. 
+            You only need to add the xxx/jdt-language-server/bin path to the system environment variable path.\033[0m""")
+            else:
+                print(f"""\033[1;31m[CHECK] {lang} command is invalid \u2718\033[0m""")
+        except subprocess.TimeoutExpired:
+            print(f"\033[1;32m[CHECK] {lang} command is valid \u2714\033[0m")
+    
+    app = web.Application([
+        (r"/", HomeRequestHandler, dict(commands=config['commands'])),
+        (r"/file", FileServerWebSocketHandler,
+         dict(rootUri=rootUri)),
+        (r"/(.*)", LanguageServerWebSocketHandler,
+         dict(commands=config['commands']))
+    ])
+
+    print("\033[1;34m\nStarted Web Socket at:\n" + "\n".join(
+        ["  - {}: ws://{}:{}/{}".format(lang, config['host'],
+                                        config['port'], lang) for lang in config['commands'].keys()])
+    + "\033[0m")
+
+    server = httpserver.HTTPServer(app)
+
+    server.listen(config['port'])
+    ioloop.IOLoop.current().start()
