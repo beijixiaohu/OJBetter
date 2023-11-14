8af227bbf4ce5b043d4bad974dac7c3f0870e9a3
beijixiaohu <yuhao888123@gmail.com>
2023-11-14 17:09:03 +0800

    Update LICENSE For Script

--- script/Atcoder Better!.js
+++ script/Atcoder Better!.js
@@ -27,7 +27,7 @@
 // @require      https://cdn.staticfile.org/turndown/7.1.2/turndown.min.js
 // @require      https://cdn.staticfile.org/markdown-it/13.0.1/markdown-it.min.js
 // @require      https://cdn.bootcdn.net/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
-// @license      MIT
+// @license      GPL3
 // @compatible	 Chrome
 // @compatible	 Firefox
 // @compatible	 Edge
