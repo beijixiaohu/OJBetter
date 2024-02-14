8bae3f1c266d5b20c2fa864a34c8fa311c7e2db3
beijixiaohu <yuhao888123@gmail.com>
2024-02-14 16:53:08 +0800

    chore: 添加版本号文件生成和脚本同步至 oss 的 action

---
+++ .github/scripts/update-version.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const directories = {
+  dev: 'script/dev',
+  release: 'script/release'
+};
+
+let versions = {};
+
+Object.keys(directories).forEach((key) => {
+  const dirPath = directories[key];
+  const files = fs.readdirSync(dirPath);
+
+  files.forEach((file) => {
+    if (path.extname(file) === '.js') {
+      const content = fs.readFileSync(path.join(dirPath, file), 'utf8');
+      const versionMatch = content.match(/\/\/ @version\s+(.*)/);
+      if (versionMatch) {
+        const name = file.replace('.user.js', '').replace(/\s+/g, '');
+        if (!versions[name]) {
+          versions[name] = {};
+        }
+        versions[name][key] = versionMatch[1];
+      }
+    }
+  });
+});
+
+fs.writeFileSync('versions.json', JSON.stringify(versions, null, 2));
