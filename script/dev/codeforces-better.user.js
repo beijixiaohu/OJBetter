d535d45e0e673bcf42748b6711c7f8a5b21c1866
beijixiaohu <yuhao888123@gmail.com>
2024-05-05 16:36:18 +0800

    chore(Codeforces): 重新生成版本文件

--- script/dev/codeforces-better.user.js
+++ script/dev/codeforces-better.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Codeforces Better!
 // @namespace    https://greasyfork.org/users/747162
-// @version      1.74.15
+// @version      1.74.16
 // @author       北极小狐
 // @match        *://*.codeforces.com/*
 // @match        *://*.codeforc.es/*
