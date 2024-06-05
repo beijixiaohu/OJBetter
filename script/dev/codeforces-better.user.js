bd703d26f4938280a736c99378c0d33bba31f9f1
beijixiaohu <yuhao888123@gmail.com>
2024-06-05 11:20:07 +0800

    release(Codeforces): Release version 1.75.0

--- script/dev/codeforces-better.user.js
+++ script/dev/codeforces-better.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Codeforces Better!
 // @namespace    https://greasyfork.org/users/747162
-// @version      1.74.20
+// @version      1.75.0
 // @author       北极小狐
 // @match        *://*.codeforces.com/*
 // @match        *://*.codeforc.es/*
