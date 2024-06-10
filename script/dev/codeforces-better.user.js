2e0661a3ffb54ff66b26eefe184d88a8f668595c
北极小狐 <60805870+beijixiaohu@users.noreply.github.com>
2024-06-10 20:46:52 +0800

    Update codeforces-better.user.js

--- script/dev/codeforces-better.user.js
+++ script/dev/codeforces-better.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Codeforces Better!
 // @namespace    https://greasyfork.org/users/747162
-// @version      1.75.1
+// @version      1.75.2
 // @author       北极小狐
 // @match        *://*.codeforces.com/*
 // @match        *://*.codeforc.es/*
