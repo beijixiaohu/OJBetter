0e6f689c88800ed587551fccc8dd55953bf19f98
wrk-123 <83344959+wrk-123@users.noreply.github.com>
2025-08-29 17:11:56 +0800

    Update codeforces-better.user.js

--- script/dev/codeforces-better.user.js
+++ script/dev/codeforces-better.user.js
@@ -11079,7 +11079,7 @@ async function CF2luogu(problemToolbar) {
           method: "GET",
           url,
         });
-        return response.status<300&&!response.responseText.match(/出错了/g);
+        return response.status<300&&!response.responseText.match(/出错了/g);//匹配 1xx 和 2xx
       },
       {
         maxRetries: 3,
@@ -17216,3 +17216,4 @@ if (document.readyState === "loading") {
     location.reload();
   }
 }
+
