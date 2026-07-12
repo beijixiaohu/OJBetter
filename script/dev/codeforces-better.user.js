5a006a222c786db22fef3189886cb9c7131fab1f
beijixiaohu <beijixiaohu@users.noreply.github.com>
2026-07-12 11:15:56 +0000

    Update script versions

--- script/dev/codeforces-better.user.js
+++ script/dev/codeforces-better.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Codeforces Better!
 // @namespace    https://greasyfork.org/users/747162
-// @version      1.86.0
+// @version      1.86.1
 // @author       北极小狐
 // @match        *://*.codeforces.com/*
 // @match        *://*.codeforc.es/*
